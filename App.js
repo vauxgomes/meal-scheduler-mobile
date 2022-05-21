3bcafdf62fa7bcf10abfcf500dec437766d23ece
vaux.gomes <vauxsandino@gmail.com>
2022-05-21 16:56:06 -0300

    Drafting Meals View

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  ScrollView,
+  Image,
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+
+import {
+  BG,
+  BG_SECONDARY,
+  FS_LG,
+  FS_XL,
+  PRIMARY,
+  SPACING_LG,
+  SPACING_MD,
+} from "./src/styles/styles";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          style={{ width: 24, height: 24 }}
+          source={require("./assets/icons/bolt-circle.png")}
+        />
+        <Text style={styles.title}>Jandaya</Text>
+      </View>
+
+      <ScrollView style={styles.body}></ScrollView>
+
+      <TouchableOpacity style={styles.floatingButton}>
+        <Image
+          style={{ width: 24, height: 24 }}
+          source={require("./assets/icons/qr-scan.png")}
+        />
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: BG,
+    position: "relative",
+  },
+
+  header: {
+    padding: SPACING_LG,
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: BG,
+  },
+
+  title: {
+    color: PRIMARY,
+    fontSize: FS_LG,
+    marginLeft: SPACING_MD,
+  },
+
+  body: {
+    backgroundColor: "white",
+    padding: SPACING_LG,
+  },
+
+  floatingButton: {
+    position: "absolute",
+    bottom: 25,
+    right: 25,
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    backgroundColor: BG_SECONDARY,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/styles/styles.js
@@ -0,0 +1,37 @@
+// Typography
+export const FONT_FAMILY = "Poppins";
+
+// Font Sizes
+export const FS_XXS = 9;
+export const FS_XS = 10;
+export const FS_SM = 13;
+export const FS_MD = 16;
+export const FS_LG = 18;
+export const FS_XL = 24;
+export const FS_XXL = 30;
+
+// Background Colors
+export const BG = "#1f1f21";
+export const BG_SECONDARY = "#454344";
+
+// Colors
+export const PRIMARY = "#ffffff";
+export const PRIMARY_LIGHT = "#f7fcf9";
+
+export const SECONDARY = "#f5aa2e";
+export const SECONDARY_LIGHT = "#fffbf7";
+
+export const GRAY = "#9fa4b2";
+export const GRAY_LIGHT = "#f5f7fa";
+export const GRAY_MEDIUM = "#bcbec4";
+export const GRAY_DARK = "#424242";
+export const GRAY_LINE = "#eaedf3";
+
+export const WHITE = "#ffffff";
+
+// Spacing
+export const SPACING_XS = 4;
+export const SPACING_SM = 8;
+export const SPACING_MD = 16;
+export const SPACING_LG = 24;
+export const SPACING_XG = 48;
